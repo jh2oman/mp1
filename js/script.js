77ebd3831258cb3b2f8227296746bc382d0891fe
Jacob Waterman <jwaterm2@illinois.edu>
2015-02-20 05:51:32 +0000

    mp1 complete

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function highlightThis(event) {
 var mn = $(".main-nav");
 var logo= $(".logo");
 $(window).scroll(function(){
-	if( $(this).scrollTop() > 400){
+	if( $(this).scrollTop() > 300){
 		mn.addClass("main-nav-scrolled");
     logo.addClass("logo-scrolled")
 	}
@@ -29,3 +29,46 @@ $(window).scroll(function(){
 	}
 });
 
+//Carousel
+//source http://agelber.com/blog/looping-carousel/
+$(function() {
+  var currentIndex = 0,
+      itemCount    = $('.carousel > li').length;
+  $('.carousel > li:eq(' + currentIndex + ')').addClass('active');
+  $('.carousel-nav').on('click', function() {
+    var $active  = $('.carousel > li.active'),
+        isNext   = $(this).hasClass('next');
+    currentIndex = (currentIndex + (isNext ? 1 : -1)) % itemCount;
+
+    //at negative 1 loop back
+    if (currentIndex === -1) {
+      currentIndex = itemCount - 1;
+    }
+    //setting up auto timer
+    var $next = $('.carousel > li:eq(' + currentIndex + ')');
+    $active.addClass(isNext ? 'next-out' : 'prev-out');
+    $next.addClass('active').addClass(isNext ? 'next-in' : 'prev-in');
+    setTimeout(function() { 
+      $active.removeClass('active next-out prev-out');
+      $next.removeClass('next-in prev-in');
+    }, 500);
+    return false;
+  });
+});
+
+//smooth scrolling
+//source http://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var link = $(this.hash);
+      link = link.length ? link : $('[name=' + this.hash.slice(1) +']');
+      if (link.length) {
+        $('html,body').animate({
+          scrollTop: link.offset().top
+        }, 750);
+        return false;
+      }
+    }
+  });
+});
