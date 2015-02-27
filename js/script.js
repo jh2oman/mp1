dcfd916ec42ae638b4660fc6db5a25a334a992ff
Jacob Waterman <jwaterm2@illinois.edu>
2015-02-27 23:07:54 +0000

    fixing highlighting

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
-
 //creating sticky nav bar
 var mn = $(".main-nav");
 var logo= $(".logo");
